8d58e9f28cf75d48483a6cac2035086f0d04aaf3
ZaidanFadillah <122280370+ZaidanFadillah@users.noreply.github.com>
2023-01-19 16:28:18 +0700

    Add files via upload

---
+++ PreviewPemesanan.java
@@ -0,0 +1,26 @@
+package PemesananKursiBioskop;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PreviewPemesanan extends JFrame {
+    private JButton BACKButton;
+    private JButton NEXTButton;
+    private JPanel mainPanel2;
+
+    public PreviewPemesanan() {
+        super("Aplikasi Pemesanan Tiket Bioskop");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(450, 720);
+        this.setContentPane(mainPanel2);
+        PemesananKursi pemesanankursi = new PemesananKursi();
+        BACKButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                mainPanel2.setVisible(false);
+                pemesanankursi.setVisible(true);
+            }
+        });
+    }
+}
